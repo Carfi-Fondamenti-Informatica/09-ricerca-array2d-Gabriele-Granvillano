c949f192b0788ffc37f1c2c1c83d6e5b2f405e2d
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-12-16 16:37:31 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,13 @@
+//
+// Created by Hp on 15/12/2022.
+//
 
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+
+void riempi(char [10][20]);
+void riempi2(char[20]);
+bool vero_falso(char[10][20], char [10]);
+void analisi(char a[10][20], char b[20]);
+
+#endif //ESERCITAZIONE_9_LIB_H
