3ad4daad2b12a17edbb02a1e2f6b4832a79cf193
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-12-16 16:36:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main()
+{
+    char elenco[10][20];
+    char nome_proprio[20];
+    riempi(elenco);
+    riempi2(nome_proprio);
+    vero_falso(elenco, nome_proprio);
+    analisi(elenco, nome_proprio);
+    return 0;
 }
